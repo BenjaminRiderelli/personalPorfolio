6f54a97ef06e92ef19445741d3f09e35b8e26c09
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-08-17 20:17:16 +0200

    small font-color bug fixed

--- src/components/formsection/index.tsx
+++ src/components/formsection/index.tsx
@@ -64,9 +64,9 @@ const FormSection = () => {
     <section className="flex flex-wrap-reverse md:gap-20 lg:gap-80 py-8 justify-center items-center min-h-screen bg-light-bg-color dark:bg-dark-bg-color border-2 border-light-text-color dark:border-dark-text-color">
       <div className="w-3/4 h-[500px] md:w-1/2 lg:w-1/3 md:min-h-[600px] border-2 border-light-text-color dark:border-dark-text-color">
         <form
-          className={`flex flex-col items-start justify-between md:justify-around w-full h-full p-2 pt-8 text-light-text-color dark:text-dark-text-color`}
+          className={`flex flex-col items-start justify-between md:justify-around w-full h-full p-2 pt-8 text-dark-text-color dark:text-light-text-color`}
         >
-          <div className="flex flex-col w-full ">
+          <div className="flex flex-col w-full text-light-text-color dark:text-dark-text-color  ">
             <label htmlFor="name">Name</label>
             <input
               value={formData.name}
@@ -77,7 +77,7 @@ const FormSection = () => {
               type="text"
             />
           </div>
-          <div className="flex flex-col w-full ">
+          <div className="flex flex-col w-full text-light-text-color dark:text-dark-text-color">
             <label htmlFor="contact">Email</label>
             <input
               value={formData.email}
@@ -88,7 +88,7 @@ const FormSection = () => {
               type="email"
             />
           </div>
-          <div className="flex flex-col w-full ">
+          <div className="flex flex-col w-full text-light-text-color dark:text-dark-text-color">
             <label htmlFor="message">Message</label>
             <textarea
               id="message"
@@ -116,7 +116,7 @@ const FormSection = () => {
                 e.preventDefault();
                 handleSubmit(formData);
               }}
-              className="border-2 border-light-bg-color dark:border-dark-text-color w-1/2 active:actionshrink text-2xl"
+              className="border-2 border-light-bg-color dark:border-dark-text-color w-1/2 active:actionshrink text-2xl text-light-text-color dark:text-dark-text-color"
             >
               {isPosting ? "Sending..." : "Send"}
             </button>
