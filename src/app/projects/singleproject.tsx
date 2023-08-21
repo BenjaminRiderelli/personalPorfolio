e059bf4fc6be0ec1456621b8956320aa97a58d87
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-08-21 17:41:04 +0200

    stash

--- src/app/projects/singleproject.tsx
+++ src/app/projects/singleproject.tsx
@@ -1,6 +1,3 @@
-"use client";
-import { useState, useEffect } from "react";
-import { Skeleton } from "@mui/material";
 import { BsGithub, BsCodeSlash } from "react-icons/bs";
 import Carousel from "../../components/carousel/carousel";
 
@@ -17,82 +14,32 @@ interface ProjectProps {
   row: number;
 }
 
-const fetchData = async (row: number) => {
+export const fetchData = async (row: number) => {
   const response = await fetch(`/api/getprojectbyid?id=${row}`);
   const data = await response.json();
+  console.log(data);
   return data;
 };
 
 export default async function Project({ row }: ProjectProps) {
-  
+
   const data = await fetchData(row);
   const [name, description, pics, techStackArr, workingLink, githubLink] =
     data.body.data.values[0];
-
-  const testData = {
+    
+  const testData:SheetData = {
     name,
     description,
-    pics,
-    techStackArr,
+    pics: JSON.parse(pics) ?? [],
+    techStack: JSON.parse(techStackArr) ?? [],
     workingLink,
     githubLink,
   };
-
-  const techStack = testData.techStackArr.map((tech) => (
+  const techStack = testData.techStack.map((tech: string) => (
     <li key={tech}>
       <p className="text-lg">{tech}</p>
     </li>
   ));
-
-  if (isLoading) {
-    return (
-      <section
-        className={`flex flex-col ${
-          row % 2 == 0 ? "lg:flex-row" : "lg:flex-row-reverse"
-        } h-full w-full border-b-2 border-light-text-color dark:border-dark-text-color `}
-      >
-        <div className="flex items-center justify-center bg-light-bg-color dark:bg-dark-bg-color w-full min-h-screen lg:w-3/4">
-          <Skeleton width="90%" height="100vh" />
-        </div>
-        <div
-          className={`flex flex-col w-full min-h-screen lg:w-1/2 bg-light-bg-color dark:bg-dark-bg-color ${
-            row % 2 == 0 ? "lg:border-l-2" : "lg:border-r-2"
-          } border-light-text-color dark:border-dark-text-color`}
-        >
-          <div className="flex flex-col lg:flex-row lg:border-b-2 border-light-text-color dark:border-dark-text-color h-full lg:h-1/4 w-full">
-            <div className="flex pt-8 lg:pt-0 items-center justify-center w-full lg:w-3/4  border-t-2 lg:border-r-2 lg:border-t-0  border-light-text-color dark:border-dark-text-color pl-8">
-              <Skeleton width="85%" height="100%" />
-            </div>
-            <ul className="flex flex-col md:flex-row lg:flex-col w-1/3 justify-center p-8 gap-4">
-              <Skeleton width={"100%"} />
-              <Skeleton width={"100%"} />
-              <Skeleton width={"100%"} />
-              <Skeleton width={"100%"} />
-            </ul>
-          </div>
-
-          <div className="flex flex-col justify-between gap-8 h-full w-full p-8 ">
-            <Skeleton height="100vh" />
-
-            <div className="flex gap-16 text-light-text-color dark:text-dark-text-color">
-              <span className="flex gap-4 items-center cursor-pointer">
-                <BsCodeSlash />
-                <a target="_blank" href={testData.workingLink}>
-                  Live Link
-                </a>
-              </span>
-              <span className="flex gap-4 items-center cursor-pointer">
-                <BsGithub />
-                <a target="_blank" href={testData.githubLink}>
-                  Github Link
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
 
   return (
     <>
@@ -103,7 +50,7 @@ export default async function Project({ row }: ProjectProps) {
       >
         <div className="flex items-center  bg-light-bg-color dark:bg-dark-bg-color w-full min-h-screen lg:w-3/4">
           <Carousel autoSlide={false} autoSlideInterval={3000}>
-            {testData.pics.map((image) => {
+            {testData.pics.map((image: string) => {
               return (
                 <img
                   style={{ objectFit: "cover" }}
