369985db59236cf1babd080245e14a63a56ee782
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-08-22 13:43:22 +0200

    asd

--- src/app/api/getprojectbyid/route.ts
+++ src/app/api/getprojectbyid/route.ts
@@ -1,17 +1,12 @@
 import { google } from "googleapis";
+import { url } from "inspector";
 import { NextRequest, NextResponse } from "next/server";
 import { URL } from "url";
 
 export async function GET(req: NextRequest, res: NextResponse) {
- 
- 
-  if (req.method !== "GET") {
-    NextResponse.json({ message: "just for getting", status: 405 });
-  }
-
   try {
     const { searchParams } = new URL(req.url as string);
-    
+
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
