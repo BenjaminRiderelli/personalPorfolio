ac6fea3dd34467255e7c32642e41a8049879d57a
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-08-22 16:06:20 +0200

    asdasd

--- src/app/projects/singleproject.tsx
+++ src/app/projects/singleproject.tsx
@@ -17,6 +17,7 @@ interface ProjectProps {
 export const fetchData = async (row: number) => {
   const response = await fetch(`https://benjaminriderelli.vercel.app/api/getprojectbyid?id=${row}`)
   const data = await response.json();
+  console.log("DATAAAAAAAA->",data)
   return data.body.data
     ? data.body.data.values[0]
     : ["", "", JSON.stringify([]), JSON.stringify([]), "", ""];
