843dd93154696c89cb491f67d6287968dbc28702
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-10-18 19:14:21 +0200

    deployment issues

--- src/components/projectssection/projectcard.tsx
+++ src/components/projectssection/projectcard.tsx
@@ -19,7 +19,6 @@ interface ProjectProps {
 export const fetchData = async (row: number) => {
   const response = await fetch(
     `https://benjaminriderelli.vercel.app/api/projects/${row}`,
-    // `http://localhost:3000/api/projects/${row}`,
     {
       next: { revalidate: 0 },
     }
