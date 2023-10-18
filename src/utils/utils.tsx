01229c28bf3d45a2335c71657a2c9fc5d5ae4bc7
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-10-18 18:15:30 +0200

    testing deploy

---
+++ src/utils/utils.tsx
@@ -0,0 +1,22 @@
+export const fetchData = async (row: number) => {
+    const response = await fetch(
+      `https://benjaminriderelli.vercel.app/api/projects/${row}`,
+      {
+        next: { revalidate: 0 },
+      }
+    );
+    const data = await response.json();
+    const finalData = data.body.data
+      ? data.body.data.values[0]
+      : [
+          "data not found x_x",
+          "There has been a problem with our server",
+          JSON.stringify([]),
+          JSON.stringify([]),
+          "",
+          "",
+        ];
+  
+    return finalData;
+  };
+  
